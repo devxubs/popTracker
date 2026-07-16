f5a943d5a02a3cc9b25aa28b92d3bd2d382683bb
Jubayer Ahmed <devxub@gmail.com>
2026-07-17 02:49:54 +0600

    first commit

--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -2,13 +2,14 @@ import { createTheme, ThemeOptions } from "@mui/material/styles";
 import typography from "./typography";
 import { lightShadows, darkShadows } from "./shadow";
 import darkPalette from "./darkPalette";
-import lightPalette from "./lightPalette";
+// import lightPalette from "./lightPalette";
 
 export const getAppTheme = (mode: "light" | "dark" = "dark") =>
    createTheme({
       palette: {
-         ...(mode === "light" ? { ...lightPalette } : { ...darkPalette }),
-         mode,
+         ...darkPalette,
+         // ...(mode === "light" ? { ...lightPalette } : { ...darkPalette }),
+         mode: "dark",
       },
       typography,
       shape: { borderRadius: 12 },
